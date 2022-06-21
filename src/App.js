04179ed8c50a219d4907584e40a94f36e3a60315
Victor Onyejide <victor.onyejide@gmail.com>
2022-06-21 12:22:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import HorizontalSlider from './component/HorizontalSlider';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  const images = ["pic1.png","pic1.png","pic1.png","pic1.png","pic1.png","pic1.png","pic1.png","pic1.png"]
+  const images2 = ["app_logo.png", "app_logo.png","app_logo.png","app_logo.png","app_logo.png","app_logo.png","app_logo.png","app_logo.png"]
+
+  const scroll = () =>{
+    const slider = document.querySelectorAll('.slider-inner');
+    const btn_pre = document.querySelectorAll('.pre');
+    const btn_next = document.querySelectorAll('.next');
+
+    slider.forEach((slide) => {
+      slide.addEventListener('wheel', (e) => {
+        e.preventDefault()
+        slide.parentElement.scrollLeft += e.deltaY; 
+        
+      })
+    })
+
+    btn_pre.forEach((pre) => {
+      pre.addEventListener('click', () =>{
+       
+        let move = pre.parentElement.querySelector('.slider');
+        move.scrollLeft -= 110
+
+      })
+    })
+
+    btn_next.forEach((next) => {
+      next.addEventListener('click', () => {
+        let move = next.parentElement.querySelector('.slider');
+        
+        move.scrollLeft += 110
+      })
+    })
+}
+
+
+
+useEffect (()=>{
+    scroll()
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+      <HorizontalSlider  folderName="pictures1" fileName={images} />
+
+       <HorizontalSlider  folderName="pictures2"  fileName={images2} spaceBetween="20px" top="50px"/>
+
     </div>
   );
 }
---
+++ src/component/HorizontalSlider.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Styled component
+const SliderWrapper = styled.div.attrs({
+    className: 'slider-wrapper'
+    }) `
+    margin-top: ${props => props.top || "0"};
+    margin-left: ${props => props.left || "0"};
+    position: relative;
+    width: ${props => props.width || "100%" };
+    height: 300px;
+    
+`
+const Slider = styled.div.attrs({
+    className: 'slider'
+    })`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    overflow-y: hidden;
+    ::-webkit-scrollbar {
+        width: 2rem;
+    }
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.bgSliderTrack || "#CBF4C0"} ;
+        border-radius: 100vw;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.bgSliderThumb ||"#FD89FF" } ;
+        border-radius: 100vw;
+       
+    }
+    
+    
+
+`
+const SliderInner = styled.div.attrs({
+    className: 'slider-inner',
+    })`
+    display: flex;
+    width: 1000px;
+    height: 100%;
+    display: flex;
+    padding: 0 40px;
+    position: relative;
+`
+const Item = styled.div.attrs({
+    className: 'item',
+    })`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 250px;
+    height: 90%;
+    margin: 0 ${props=> props.spaceBetween || "50px"};
+    top: 1%;
+    border: 1px solid black;
+`
+
+
+
+export default function HorizontalSlider ({ top, left, spaceBetween, folderName, fileName,width}) {
+    const images = fileName || []
+    const folder = folderName || ''
+    const list = images.map((file, index)=><li key={index} style={{listStyle:"none"}}><Item spaceBetween ={spaceBetween} >
+        <img src={require(`../asset/${folder}/${file}`)} style={{width: "100%"}} alt="gallery" />
+    </Item></li>)
+
+
+
+
+
+    return(
+        <SliderWrapper  width={width} top = {top} left = {left} >
+            <button className="pre" ><i class="fas fa-angle-left"></i></button>
+            <button className="next"><i class="fas fa-angle-right"></i></button>      
+            <Slider>
+                <SliderInner>
+
+                    {list}
+ 
+                </SliderInner>
+            
+
+            </Slider>
+
+        </SliderWrapper>
+
+    )
+
+
+}
+
+
+                                /* <img src={require('../asset/pictures1/pic1.png')} style={{width: "100%"}} alt="gallery" /> */
+                                /* {ab_path = path + c } */
+                                /* <img src={require('../asset/pictures1/pic1.png')} style={{width: "100%"}} alt="gallery" /> */
+
+                                // justify-content: space-evenly;
+            // <button className="next" onClick={()=> document.querySelector('.slider-inner').parentElement.scrollLeft +=110 }><i class="fas fa-angle-right"></i></button>      
+                                
